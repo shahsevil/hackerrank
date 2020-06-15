39e5b8c8fe8d157bd744b2d1796e9f4fae1e7112
shahsevil <shahsevil@gmail.com>
2020-06-15 23:03:41 +0400

    -- SolveMeFirst

---
+++ src/main/java/ProblemSolving/warmup/SolveMeFirst.java
@@ -0,0 +1,14 @@
+package ProblemSolving.warmup;
+
+public class SolveMeFirst {
+    static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int sum = solveMeFirst(a, b);
+        System.out.println(sum);
+    }
+}
