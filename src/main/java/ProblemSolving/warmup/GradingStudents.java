de4d2ce322b17632fb6f70b4e1e20dfb2dabdfa8
shahsevil <shahsevil@gmail.com>
2020-06-16 02:10:20 +0400

    -- GradingStudents

--- src/main/java/ProblemSolving/warmup/GradingStudents.java
+++ src/main/java/ProblemSolving/warmup/GradingStudents.java
@@ -14,6 +14,6 @@ public class GradingStudents {
     }
 
     public static void main(String[] args) {
-        System.out.println(gradingStudents(Arrays.asList(73,67,38 ,33)));
+        System.out.println(gradingStudents(Arrays.asList(73, 67, 38, 33)));
     }
 }
